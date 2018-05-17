b3766ebf8f47f24747061cbf631a9ad53ed15d7c
godchurch <37278203+godchurch@users.noreply.github.com>
2018-05-17 13:48:29 -1000

    modified environment

--- sh/profile.sh
+++ sh/profile.sh
@@ -1,59 +1,62 @@
-if [ "${myEnvironment}" = 1 ]; then
+if [ "${my_environment}" = 1 ]; then
   return 0
 fi
 
-removePath() {
-  local inputPath="${inputPath:-$1}"
-  local customPath=''
+remove_path() {
+  local input_path="${input_path:-$1}"
+  local custom_path=""
   case "${PATH}" in
-    *${inputPath}*)
-      local IFS=':'
-      local directory=''
+    *${input_path}*)
+      local IFS=":"
+      local directory=""
       for directory in ${PATH}; do
-        if [ "${directory}" != "${inputPath}" ]; then
-          customPath="${customPath:+${customPath}:}${directory}"
+        if [ "${directory}" != "${input_path}" ]; then
+          custom_path="${custom_path:+${custom_path}:}${directory}"
         fi
       done
       ;;
   esac
-  echo "${customPath:-${PATH}}"
+  echo "${custom_path:-${PATH}}"
 }
 
-prependPath() {
-  local inputPath="$1"
-  local customPath="$(removePath)"
-  echo "${inputPath}:${customPath}"
+prepend_path() {
+  local input_path="$1"
+  local customPath="$(remove_path)"
+  echo "${input_path}:${custom_path}"
 }
 
-appendPath() {
-  local inputPath="$1"
-  local customPath="$(removePath)"
-  echo "${customPath}:${inputPath}"
+append_path() {
+  local input_path="$1"
+  local customPath="$(remove_path)"
+  echo "${custom_path}:${input_path}"
 }
 
 if [ -d "${HOME}/.local/bin" ]; then
-  PATH=$(prependPath "${HOME}/.local/bin")
+  PATH="$(prepend_path "${HOME}/.local/bin")"
   export PATH
 fi
 
-export myEnvironment=1
+my_environment=1
+export my_environment
 
 LS_COLORS="di=1:fi=0:ln=36:pi=33;40:so=35:bd=33;40:cd=33;40:or=31:mi=0:ex=32"
 export LS_COLORS
 
-export XDG_DATA_HOME="${HOME}/.local/share"
-export XDG_CONFIG_HOME="${HOME}/.config"
-export XDG_CACHE_HOME="${HOME}/.cache"
+XDG_DATA_HOME="${HOME}/.local/share"
+XDG_CONFIG_HOME="${HOME}/.config"
+XDG_CACHE_HOME="${HOME}/.cache"
+export XDG_DATA_HOME XDG_CONFIG_HOME XDG_CACHE_HOME
 
-export LESSHISTFILE="-"
+LESSHISTFILE="-"
+export LESSHISTFILE
 
 unset EDITOR
-if [ -x /usr/bin/vim ]; then
-  EDITOR=/usr/bin/vim
-elif [ -x /usr/bin/vi ]; then
-  EDITOR=/usr/bin/vi
-elif [ -x /bin/nano ]; then
-  EDITOR=/bin/nano
+if [ -x "/usr/bin/vim" ]; then
+  EDITOR="/usr/bin/vim"
+elif [ -x "/usr/bin/vi" ]; then
+  EDITOR="/usr/bin/vi"
+elif [ -x "/bin/nano" ]; then
+  EDITOR="/bin/nano"
 fi
 
 export EDITOR="${EDITOR}"
